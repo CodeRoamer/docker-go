dc5e4b6a83fdaa56c76f18c34043d509a04f9dbd
Li He <jet.in.brain@gmail.com>
2014-07-21 10:02:51 +0800

    merge conflict

--- api/dclient_test.go
+++ api/dclient_test.go
@@ -2,46 +2,13 @@ package api
 
 import "testing"
 
-//func TestDClient_do(t *testing.T) {
-//	client, err := NewDClient("unix://var/run/docker.sock", "v1.13", 20)
-//	if err != nil {
-//		t.Error(err)
-//		return
-//	}
-//	res, status, err := client.do("GET", "/images/json", "application/json", nil)
-//	if err != nil {
-//		t.Error(err)
-//		return
-//	}
-//	if status == 404 {
-//		t.Error("404 request error")
-//		return
-//	}
-//	if res == nil {
-//		t.Error("request empty")
-//	}
-//
-//}
-
-type People struct {
-	Hello string `json:"hello"`
-	Name string `json:"name"`
-	Age int `json:"age"`
-}
-
-func TestGet(t *testing.T) {
-	client, err := NewDClient("http://127.0.0.1:8000/", "api",20)
-
+func TestDClient_do(t *testing.T) {
+	client, err := NewDClient("http://42.96.195.83:4213", "v1.13")
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	status, err := client.get("/announcement", People{
-		Hello: "name",
-		Name: "lihe",
-		Age: 18,
-	})
-
+	res, status, err := client.do("GET", "/images/json", "application/json", nil)
 	if err != nil {
 		t.Error(err)
 		return
@@ -50,4 +17,8 @@ func TestGet(t *testing.T) {
 		t.Error("404 request error")
 		return
 	}
+	if res == nil {
+		t.Error("request empty")
+	}
+
 }
