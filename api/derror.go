de34731cc256b5a464df066d2ba3a8c66cf0c1db
Li He <jet.in.brain@gmail.com>
2014-07-21 09:54:48 +0800

    add APIError for docker-go

--- api/derror.go
+++ api/derror.go
@@ -1,42 +1,20 @@
 package api
 
-const (
-	ReqError = "request error"
-	NoError = "Success"
-)
 
-func GetGeneralStatusError(code int, module *ModuleAPI) (res string) {
-	switch code{
-	case 200, 204:
-		res = NoError
-	case 500:
-		res = "server error"
-	case 400:
-		res = "bad parameter"
-	case 404:
-		if module.Module == Containers {
-			res = "no such container or request 404"
-		}else if module.Module == Images {
-			res = "no such image or request 404"
-		}else {
-			res = "no possible error"
-		}
-	case 406:
-		if module.Module == Containers {
-			res = "impossible to attach (container not running)"
-		}else {
-			res = "no possible error"
-		}
-	case 304:
-		if module.Module == Containers && module.Api == Start {
-			res = "container already started"
-		}else if module.Module == Containers && module.Api == Stop {
-			res = "container already stopped"
-		}
-	case 409:
-		res = "conflict"
-	default:
-		res = "no possible error"
-	}
-	return
+
+type APIError struct {
+	ErrorMessage string
+	StatusCode int
+}
+
+func (err APIError) Error() string {
+	return string(err.StatusCode)+": "+err.ErrorMessage
+}
+
+func (err APIError) IsClientError() bool {
+	return err.StatusCode >= 400 && err.StatusCode < 500
+}
+
+func (err APIError) IsClientError() bool {
+	return err.StatusCode >= 500 && err.StatusCode < 600
 }
